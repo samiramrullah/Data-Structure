1fdf40a3c84538bf997ffcf1ce35b94e67287c04
samiramrullah <samiramrullah@gmail.com>
2022-08-24 21:59:02 +0530

    recursion factorial

--- Recursion/printnameNtime.cpp
+++ Recursion/printnameNtime.cpp
@@ -1,44 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void printNtimes(int n)
 {
-    if(n<=0) return ;
-    cout<<"Samir\n";
-    printNtimes(n-1);
+    if (n <= 0)
+        return;
+    cout << "Samir\n";
+    printNtimes(n - 1);
 }
-void printuptoN(int n,int i=0)
+void printuptoN(int n, int i = 0)
 {
-    if(i>n) return ;
-    cout<<i<<" ";
-    printuptoN(n,(i+1));
+    if (i > n)
+        return;
+    cout << i << " ";
+    printuptoN(n, (i + 1));
 }
 void printOpposite(int n)
 {
-    if(n<0) return;
-    cout<<n<<" ";
-    printOpposite(n-1);
+    if (n < 0)
+        return;
+    cout << n << " ";
+    printOpposite(n - 1);
 }
 void printBacktracking(int n)
 {
-    if(n<=0) return;
-    printBacktracking(n-1);
-    cout<<(n);
+    if (n <= 0)
+        return;
+    printBacktracking(n - 1);
+    cout << (n);
 }
 int printSumuptoN(int n)
 {
-    if(n<=0) return 0;
-    return n+printSumuptoN(n-1);
+    if (n <= 0)
+        return 0;
+    return n + printSumuptoN(n - 1);
+}
+int factorial(int n)
+{
+    if(n==0) return 1;
+    return n*factorial(n-1);
 }
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     // printNtimes(n);
     // printuptoN(n);
     // printOpposite(n);
-    //backtracking
+    // backtracking
     // printBacktracking(n);
-    cout<<printSumuptoN(n);
+    // cout << printSumuptoN(n);
+    cout<<factorial(n);
     return 0;
 }
